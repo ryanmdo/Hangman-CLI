1b19c39ff140120bb0bf7c703a806aad617e0f6e
ryanmdo <ryan@libertylearner.com>
2017-12-10 22:23:56 -0800

    making sure to add all files again

--- word.js
+++ word.js
@@ -6,8 +6,6 @@ function Word(){
     var wordArr = ['arm','back','ears','eyes','face','feet','stomach','teeth','thumbs','toes','tongue','tooth','fingers','foot','hair','hands','head','knees','legs','mouth','neck','nose','shoulders','skin'];
     this.word = wordArr[Math.floor(Math.random())];;
     console.log('The random word is '+this.word)
-
-
 }
 
 
